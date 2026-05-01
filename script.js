c143f248ad8d5437570ffb67817974e2e076192a
kawabe <kawabe@.com>
2026-05-01 12:42:19 +0900

    おみくじ結果の色変更

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const fortunes=[
     "中吉　☺　少し勇気を出すとよいことが起きる",
     "小吉　今日はゆっくり過ごそう",
     "凶　無理せず慎重に行動しよう",
-    "大凶　😢　残念",
-  
+    "大凶　😢　残念", 
 ];
 
 const button = document.getElementById("a");
@@ -12,5 +11,20 @@ const result = document.getElementById("b");
 
 button.addEventListener("click", function(){
   const randomNumber = Math.floor(Math.random() * fortunes.length);
+
+　const resultText=fortunes[(randomNumber)];
+
   result.textContent=fortunes[(randomNumber)];
+  if(resultText.includes("大吉")) {
+     result.style.color="red";
+       }
+  else if(resultText.includes("大凶")) {
+     result.style.color="blue";
+       }
+  else if(resultText.includes("中吉")) {
+     result.style.color="orange";
+       }
+  else if(resultText.includes("小吉")) {
+     result.style.color="green";
+       }
 })
